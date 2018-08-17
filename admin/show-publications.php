915ec52b96a24f3131d5f1b269e65094ad7d50bc
Simon <simon@S1M-MBP.fritz.box>
2018-08-17 22:23:42 +0200

    svg buttons to remove/delete publications from a users list

--- admin/show-publications.php
+++ admin/show-publications.php
@@ -121,9 +121,10 @@ function tp_show_publications_page() {
         for( $i = 0; $i < $max; $i++ ) {
             $array_variables['checkbox'][$i] = intval($array_variables['checkbox'][$i]);
             $userbookmark = tp_bookmarks::bookmark_exists($array_variables['checkbox'][$i], $current_user->ID);
-            //if ( $userbookmark === false ) {
-                tp_bookmarks::delete_bookmark( $array_variables['checkbox'][$i], $current_user->ID );
-            //}
+            if ( $userbookmark ==! false ) {
+                tp_bookmarks::delete_bookmark( $array_variables['checkbox'][$i], $current_user->ID);
+
+            }
         }
         get_tp_message( __('Publications removed from your list','teachpress') );
     }
@@ -150,6 +151,14 @@ function tp_show_publications_page() {
     else {
         tp_publications_page::get_tab($user, $array_variables);
     }
+
+    // Show page
+    if ( $array_variables['action'] === 'apa' ) {
+        tp_publications_page::get_bibtex_screen($array_variables);
+    }
+    else {
+        tp_publications_page::get_tab($user, $array_variables);
+    }
     
     echo '</div>';
 }
@@ -160,6 +169,15 @@ function tp_show_publications_page() {
  * @since 5.0.0
  */
 class tp_publications_page {
+
+  /**
+     * APA-Export mode for show publications page
+     * @param array $array_variables
+     * @since 5.0.0
+     */
+    public static function get_apa_screen($array_variables) {
+        echo 'This is the new APA-Export function';
+    }
     
     /**
      * bibtex mode for show publications page
@@ -323,12 +341,26 @@ class tp_publications_page {
         if ( $array_variables['page'] === 'publications.php' ) {
            // Add to your own list icon
            if ($test2 === false) {
-              echo '<a href="admin.php?page=' . $array_variables['page'] . '&amp;add_id='. $row->pub_id . $get_string . '" title="' . __('Add to your own list','teachpress') . '">+</a>';
+              echo '<a href="admin.php?page=' . $array_variables['page'] . '&amp;add_id='. $row->pub_id . $get_string . '" title="' . __('Add to your own list','teachpress') . '"><?xml version="1.0" encoding="iso-8859-1"?>
+<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+   viewBox="0 0 52 52" style="enable-background:new 0 0 52 52;" xml:space="preserve">
+<path d="M26,0C11.664,0,0,11.663,0,26s11.664,26,26,26s26-11.663,26-26S40.336,0,26,0z M38.5,28H28v11c0,1.104-0.896,2-2,2
+  s-2-0.896-2-2V28H13.5c-1.104,0-2-0.896-2-2s0.896-2,2-2H24V14c0-1.104,0.896-2,2-2s2,0.896,2,2v10h10.5c1.104,0,2,0.896,2,2
+  S39.604,28,38.5,28z"/>
+</svg></a>';
            }
         }
         else {
            // Delete from your own list icon
-           echo '<a href="admin.php?page=' . $array_variables['page'] .'&amp;del_id='. $test2 . $get_string . '" title="' . __('Delete from your own list','teachpress') . '">&laquo;</a>';
+           echo '<a href="admin.php?page=' . $array_variables['page'] .'&amp;del_id='. $test2 . $get_string . '" title="' . __('Delete from your own list','teachpress') . '"><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+   viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+<g>
+  <g>
+    <path d="M256,0C114.844,0,0,114.844,0,256s114.844,256,256,256s256-114.844,256-256S397.156,0,256,0z M405.333,266.667
+      c0,5.896-4.771,10.667-10.667,10.667H117.333c-5.896,0-10.667-4.771-10.667-10.667v-21.333c0-5.896,4.771-10.667,10.667-10.667
+      h277.333c5.896,0,10.667,4.771,10.667,10.667V266.667z"/>
+  </g>
+</svg></a>';
         }
         echo '</td>';
         
